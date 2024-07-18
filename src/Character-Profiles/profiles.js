599a248261527759221790e013e61c3390664e46
mirza-zain <mirzazain@gmail.com>
2024-07-19 02:23:38 +0500

    Updated Character Page

--- src/Character-Profiles/profiles.js
+++ src/Character-Profiles/profiles.js
@@ -1,38 +1,59 @@
 import midoriya from './charImages/midoriya.webp';
 import bakugo from './charImages/bakugo.webp';
 import todoroki from './charImages/todoroki.webp';
+import uaraka from './charImages/uaraka.webp';
+import lida from './charImages/lida.webp';
+import krishma from './charImages/krishma.webp';
 
 const profiles = [
     { 
       id:1,
       image: midoriya,
-      name: "Izuku Midoriya (緑みどり谷や出いず久く Midoriya Izuku)",
-      alias: "Deku (デク Deku)", 
-      bio: "Izuku Midoriya (緑みどり谷や出いず久く Midoriya Izuku), also known as Deku (デク Deku), is the main protagonist of the My Hero Academia manga and anime series.", 
+      name: "Izuku Midoriya (緑みどり谷や出いず久く)",
+      alias: "Deku (デク)", 
       birth: "15 July",
       quirk: "One For All",
-      gender: "Male"
     },
     { 
       id:2,
       image: bakugo,
-      name: "Katsuki Bakugo (爆ばく豪ごう勝かつ己き Bakugō Katsuki)",
-      alias: "Kacchan (かっちゃん Katchan)", 
-      bio: "Katsuki Bakugo (爆ばく豪ごう勝かつ己き Bakugō Katsuki), also known as Kacchan (かっちゃん Katchan) by his childhood friends, and his hero name Great Explosion Murder God Dynamight (大だい•爆ばく•殺さっ•神しんダイナマイト Daibakusasshin Dainamaito?), is a student in Class 1-A at U.A. High School, training to become a Pro Hero. He is the deuteragonist of the series.", 
-      birth: "15 July",
-      quirk: "One For All",
-      gender: "Male"
+      name: "Katsuki Bakugo (爆ばく豪ごう勝かつ己き)",
+      alias: "Kacchan (かっちゃん)", 
+      birth: "20 April",
+      quirk: "Explosion",
     },
     { 
       id:3,
       image: todoroki,
-      name: "Shoto Todoroki (轟とどろき焦しょう凍と Todoroki Shōto)",
-      alias: "Shoto (ショート Shōto)", 
-      bio: "He is a student in Class 1-A at U.A. High School, where he got in through official recommendations and is training to become a Pro Hero. He is the youngest son of Endeavor, the former No. 2, and current No. 1, Pro Hero.", 
+      name: "Shoto Todoroki (轟とどろき焦しょう凍と)",
+      alias: "Shoto (ショート)", 
       birth: "11 January",
       quirk: "Half-Cold Half-Hot",
-      gender: "Male"
-    }
+    },
+    { 
+      id:4,
+      image: uaraka,
+      name: "Ochaco Uraraka (麗うらら日かお茶ちゃ子こ)",
+      alias: "Uravity (ウラビティ),", 
+      birth: "27 December",
+      quirk: "Zero Gravity",
+    },
+    { 
+      id:5,
+      image: lida,
+      name: "Tenya Ida (飯いい田だ天てん哉や)",
+      alias: "Turbo Hero: Ingenium (ターボヒーロー・インゲニウム)", 
+      birth: "22 August",
+      quirk: "Engine",
+    },
+    { 
+      id:6,
+      image: krishma,
+      name: "Eijiro Kirishima (切きり島しま鋭えい児じ郎)",
+      alias: "Sturdy Hero: Red Riot (剛健ヒーロー 烈レッ怒ド頼ライ雄オッ斗ト)", 
+      birth: "16 October",
+      quirk: "Hardening",
+    },
 ]
 
 export default profiles
--- src/components/Character.js
+++ src/components/Character.js
@@ -3,24 +3,22 @@ import profiles from '../Character-Profiles/profiles';
 const Character = () => {
   return (
     <div id='#character'>
+      <div className='p-5 w-full h-1/2 flex bg-gray-950 pt-24 gap-10 overflow-hidden overflow-x-scroll '>
         {
           profiles.map(pro => {
             return ( 
-          <div key={pro.id} className='flex flex-col md:flex-row bg-gray-950 pt-24'>
-            <div className='w-full md:w-1/2 bg-yellow-300 flex justify-center items-center'> 
-              <img src={pro.image} alt={pro.name} className='w-[50%] md:w-[25%] h-[100%] object-cover object-center'/> 
-            </div>
-            <div className='md:w-1/2 bg-orange-400'>
-                <h2 className='font-bold font-["Black_Ops_One"] text-center text-2xl text-red-200 my-2'>{pro.name}</h2>
-                <h2 className='text-gray-300 font-semibold text-xl mx-5 mb-2'>Alias: <span>{pro.alias}</span></h2>
-                <p className='text-gray-300 text-xl text-justify mx-5'>{pro.bio}</p>
-                <h3 className='text-gray-300 font-semibold text-xl mx-5 mb-2'>Quirk: <span>{pro.quirk}</span></h3>
-                <h3 className='text-gray-300 font-semibold text-xl mx-5 mb-2'>Birthday: <span>{pro.birth}</span></h3>
-                <h3 className='text-gray-300 font-semibold text-xl mx-5'>Gender: <span>{pro.gender}</span></h3>
-            </div>
-          </div>
-        )})
-        }
+              <div key={pro.id} className='w-full h-screen bg-gray-950'> 
+              <img src={pro.image} alt={pro.name} className='w-full h-1/2 object-cover object-top'/> 
+            {/* </div>
+            <div className='md:w-1/2 bg-orange-400'> */}
+                <h2 className='font-bold font-["Black_Ops_One"] text-center text-2xl text-red-200'>{pro.name}</h2>
+                <h2 className='text-white font-semibold text-xl mb-2'>Alias: <span>{pro.alias}</span></h2>
+                <h3 className='text-white font-semibold text-xl mb-2'>Quirk: <span>{pro.quirk}</span></h3>
+                <h3 className='text-white font-semibold text-xl mb-2'>Birthday: <span>{pro.birth}</span></h3>
+                </div>
+              )})
+            }
+      </div>
     </div>
   );
 };
